cdf235dfd92abea58f7cdb73f2ece1d813dd5042
thomasheimstad <thomasheimstad@gmail.com>
2017-11-26 21:53:38 +0100

    removed cover, styled mobile header

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,7 +150,6 @@ module.exports = {
                     fields { slug }
                     frontmatter {
                       title
-                      cover
                       date
                       category
                       tags
