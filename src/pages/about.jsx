77d658fe9c094c755e38948bf01caeabf22f28cc
thomasheimstad <thomasheimstad@gmail.com>
2017-12-06 21:02:16 +0100

    failed attempt about in markdown

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,46 +7,13 @@ import config from "../../data/SiteConfig";
 
 class AboutPage extends Component {
   render() {
-      const postEdges = this.props.data.allMarkdownRemark.edges;
     return (
       <div className="about flex center column">
         <Helmet title={`About | ${config.siteTitle}`} />
-        <Hero id="aboutPage" src={postEdges[0].node.frontmatter.thumbnail.childImageSharp.responsiveSizes.src} bgpos="50% 20%" headerSize="1.5"/>
-        <About postEdges={postEdges}/>
+        <Hero id="aboutPage" src={Einar6} bgpos="50% 20%" headerSize="1.5"/>
+        <About />
       </div>
     );
   }
 }
 export default AboutPage;
-export const query = graphql`
-  query AboutQuery {
-    allMarkdownRemark(
-      filter: { frontmatter: { category: { eq: "about"} } }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          excerpt(pruneLength: 100)
-          timeToRead
-          html
-          frontmatter {
-            title
-            category
-            thumbnail {
-              childImageSharp {
-                responsiveSizes(maxWidth: 1920, quality: 75, cropFocus: CENTER, toFormat: JPG) {
-                  src
-                  srcSet
-                  sizes
-                  base64
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
